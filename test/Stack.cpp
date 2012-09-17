69c2c1af8ad2c591e0d7a95dbb304ef0fcc9a060
Iryna <iryna@debian.com>
2012-09-17 17:46:35 -0400

    contains a unit test that tests the autoresizing of the stack

--- test/Stack.cpp
+++ test/Stack.cpp
@@ -3,9 +3,20 @@
 
 TEST(StackTest, Push) {
   Stack* s = new Stack();
-  s->push(6);
+  s -> push(6);
+  EXPECT_EQ(6, s -> peek());
+  delete s;
+}
 
-  EXPECT_EQ(6, s->peek());
+TEST(StackTest, PushOutOfBounds) {
+  Stack* s = new Stack();
+  int max = 1000;
+  for (int i = 0; i < max; ++i){
+    s->push(i);
+  }
+
+  EXPECT_EQ(max - 1, s->peek());
+
   delete s;
 }
 
